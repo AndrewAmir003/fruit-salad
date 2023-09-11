bae00407840ef3e6a9115b95a6808aa1e001b687
“AndrewAmir003” <andrewamir003@gmail.com>
2023-09-11 15:02:07 +0100

    Fetching data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const fruitForm = document.querySelector("#inputSection form")
 
 const fruitList = document.querySelector("#fruitSection ul")
 
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+
 // const deleteEvent = event => {
 //     if(!confirm("Do you wish to remove this item?")) {
 //         return
@@ -10,20 +13,46 @@ const fruitList = document.querySelector("#fruitSection ul")
 //     event.target.remove()
 // }
 
-const removeFruit = e => {
-    e.target.remove();
-  }
+const fetchFruitData = fruit => {
+    fetch(`https://fruity-api.onrender.com/api/fruits/${fruit}`) // send request to url. Once data is received then move to .then
+    .then(resp => resp.json()) // these are asynchronous functions
+    .then(data => addFruit(data))
+    .catch(e => console.error(e))
+}
+
+let totalCal = 0,
+totalCarb = 0,
+totalFat = 0,
+totalPro = 0,
+totalSug = 0
 
 const addFruit = (fruit) => {
     const li = document.createElement("li") // creating list item
-    li.textContent = fruit
+    li.textContent = fruit.name
     // li.addEventListener("click", deleteEvent)
     fruitList.appendChild(li) // add to the end of the list
-    fruitList.addEventListener('click', removeFruit)
+
+    totalCal += fruit.nutritions.calories 
+    totalCarb += fruit.nutritions.carbohydrates
+    totalFat += fruit.nutritions.fat
+    totalPro += fruit.nutritions.protein
+    totalSug += fruit.nutritions.sugar
+   
+    fruitNutrition.textContent = `Calories: ${totalCal},\n
+    Carbohydrates: ${totalCarb},\n
+    Fat: ${totalFat},\n 
+    Protein: ${totalPro},\n 
+    Sugar: ${totalSug}`
+
 }
 
 fruitForm.addEventListener("submit", e => {
     e.preventDefault()
-    addFruit(e.target.fruitInput.value)
+    fetchFruitData(e.target.fruitInput.value)
     e.target.fruitInput.value = ""
 })
+
+const removeFruit = e => {
+    e.target.remove();
+  }
+  fruitList.addEventListener('click', removeFruit)
